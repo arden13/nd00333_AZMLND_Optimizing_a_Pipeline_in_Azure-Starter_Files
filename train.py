acd2ea9f19136e0ef5ac5db81fc0918f2f5a2891
arden13 <ardenthebibliophile@gmail.com>
2021-10-14 00:33:53 +0000

    Finish class
    Absolutely terrible experience. Would not recommend this class to an enemy.

--- train.py
+++ train.py
@@ -10,17 +10,13 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
+
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+pth = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
+ds = TabularDatasetFactory().from_delimited_files(pth)
 
 run = Run.get_context()
 
@@ -49,7 +45,18 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_df, y_df
+
     
+
+# x, y = clean_data(ds)
+
+# x_train, x_test, y_train, y_test = train_test_split((x,y))
+
+# ### YOUR CODE HERE ###a
+
+# run = Run.get_context()
+
 
 def main():
     # Add arguments to script
